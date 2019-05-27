41f20135c29baf526091e0a0e43b8f73c25b6781
Danilo Moura <danilomourag@gmail.com>
2019-05-27 21:52:19 +1200

    Pagination finished

--- components/Items.js
+++ components/Items.js
@@ -4,10 +4,11 @@ import gql from 'graphql-tag'
 import styled from 'styled-components'
 import Item from './Item'
 import Pagination from '../components/Pagination'
+import { perPage } from '../config'
 
 const ALL_ITEMS_QUERY = gql`
-    query ALL_ITEMS_QUERY {
-        items {
+    query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+        items (skip: $skip, first: $first, orderBy: createdAt_DESC){
             id
             title
             price
@@ -35,11 +36,16 @@ class Items extends Component {
         return (
             <Content>
                 <Pagination page={this.props.page}></Pagination>
-                <Query query={ALL_ITEMS_QUERY}>
+                <Query 
+                    // fetchPolicy="network-only" -- avoid the cache (not recommended)
+                    query={ALL_ITEMS_QUERY}
+                    variables={{
+                        skip: (this.props.page - 1) * perPage
+                    }}>
                     {
                         ({ loading, error, data }) =>
                             (
-                                loading ? <p>Loading...</p> :
+                                loading ? <p>🏃🏼‍♂️ Loading...</p> :
                                     error ? <p>Error {error.message}</p> :
                                         <ItemsList> {data.items.map(item => <Item item={item} key={item.id} />)} </ItemsList>
                             )
